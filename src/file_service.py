cda097cf48a8cd34e2791dc9e2a63fbeca12e153
Salma Elsoly <salmaelsoly@gmail.com>
2025-04-17 01:23:43 +0200

    feat: upload file + download file + listen to coming req on peer

--- src/file_service.py
+++ src/file_service.py
@@ -1,9 +1,11 @@
+from typing import Tuple
 from dataclasses import dataclass
 import os
 from re import L
 import uuid
 import mimetypes
 from typing import List, Optional
+import socket
 
 
 SHARED_DIR = "shared"
@@ -18,11 +20,12 @@ class FileInfo:
     mime_type: Optional[str] = None
     keywords: Optional[List[str]] = None
     description: Optional[str] = None
+    is_public: bool = False
 
 class FileService:
     def __init__(self, peer):
         self.peer = peer
-        self.shared_files : dict[str, List[str]] = {}   #peer -> file_id
+        self.shared_files : dict[Tuple[str,int], List[str]] = {}   #peer -> file_id
         self.shared_files_info : dict[str, FileInfo] = {}    #file_id -> FileInfo
         self.received_files : dict[str, FileInfo] = {}
 
@@ -30,7 +33,7 @@ class FileService:
         os.makedirs(SHARED_DIR, exist_ok=True)
         os.makedirs(RECEIVED_DIR, exist_ok=True)
 
-    def share_file(self, file_path: str, keywords: Optional[List[str]] = None, description: Optional[str]=None, peer_list: Optional[List[str]] = None)-> FileInfo:
+    def share_file(self, file_path: str, keywords: Optional[List[str]] = None, description: Optional[str]=None, peer_list: Optional[List[Tuple[str, int]]] = None, is_public: bool = True)-> FileInfo:
         if not os.path.isfile(file_path):
             raise FileNotFoundError(f"File not found: {file_path}")
         file_id = str(uuid.uuid4())
@@ -41,26 +44,31 @@ class FileService:
             id=file_id,
             name=file_name,
             size=file_size,
-            owner_id=self.peer.local_peer_port,
+            owner_id=self.peer.id,
             mime_type=mime_type,
             keywords=keywords,
-            description=description
+            description=description,
+            is_public=is_public
         )
         
         with open(file_path, 'rb') as src, open(os.path.join(SHARED_DIR, file_id), 'wb') as dst:
             dst.write(src.read())
         
-        for peer in (peer_list or self.peer.peers.keys()):
+        peers_to_share = peer_list if peer_list is not None else self.peer.peers
+    
+        for peer in peers_to_share:
+            if isinstance(peer, str):  
+                peer = (peer, 0)  
             self.shared_files.setdefault(peer, []).append(file_id)
         self.shared_files_info[file_id] = file_info
         return file_info
     
    
-    def get_shared_files_by_peer(self, peer: str) -> List[FileInfo]:
+    def get_shared_files_by_peer(self, peer: Tuple[str, int]) -> List[FileInfo]:
         peer_file_ids = self.shared_files.get(peer, [])
         return [self.shared_files_info[file_id] for file_id in peer_file_ids]
     
-    def get_shared_files_by_peer_and_keywords(self, peer: str, keywords: List[str]) -> List[FileInfo]:
+    def get_shared_files_by_peer_and_keywords(self, peer: Tuple[str, int], keywords: List[str]) -> List[FileInfo]:
         peer_file_ids = self.shared_files.get(peer, [])
         return [
             self.shared_files_info[file_id]
@@ -69,8 +77,82 @@ class FileService:
                all(keyword in (self.shared_files_info[file_id].keywords or []) for keyword in keywords)
         ]
     
-    def download_file(self, file_id: str, peer: str, destination_path: str):
-        pass
+    def get_shared_files_by_peer_and_file_name(self, peer: Tuple[str, int], file_name: str) -> List[FileInfo]:
+        all_files = []
+        for peer, file_ids in self.shared_files.items():
+            for file_id in file_ids:
+                file_info = self.shared_files_info.get(file_id)
+                if file_info and file_name.lower() in file_info.name.lower():
+                    print(f"Found matching file: {file_info.name}")
+                    all_files.append(file_info)
+        print(f"Found {str(all_files)} matching files for {file_name}")
+        return all_files
+    
+    def parse_file_info(self, metadata: str) -> FileInfo:
+        if not metadata.startswith("info|"):
+            raise ValueError(f"Expected 'info|' prefix in metadata, but received: {metadata}")
+        
+        metadata_parts = metadata[5:].split("|")
+        metadata_dict = {part.split(":")[0]: part.split(":")[1] for part in metadata_parts if ":" in part}
 
-    def upload_file(self, file_path: str, peer: str):
-        pass
+        return FileInfo(
+            id=metadata_dict["id"],
+            name=metadata_dict["name"],
+            size=int(metadata_dict["size"]),
+            owner_id=metadata_dict["owner_id"],
+            mime_type=metadata_dict.get("mime_type"),
+            keywords=metadata_dict["keywords"].split(",") if "keywords" in metadata_dict and metadata_dict["keywords"] else None,
+            description=metadata_dict.get("description"),
+            is_public=metadata_dict.get("is_public", "").lower() == "true"
+        )
+    
+    def format_file_info(self, file_info: FileInfo) -> str:
+        return (
+        f"info|id:{file_info.id}|name:{file_info.name}|size:{file_info.size}|"
+        f"mime_type:{file_info.mime_type}|owner_id:{file_info.owner_id}|"
+        f"keywords:{','.join(file_info.keywords) if file_info.keywords else ''}|"
+        f"description:{file_info.description}|is_public:{file_info.is_public}"
+        )
+    
+    def download_file(self, file_id: str, socket: socket.socket):
+        try:
+            socket.sendall(f"download {file_id}\n".encode())
+
+            upload_signal = socket.recv(1024).decode().strip()
+            if upload_signal != "upload":
+                raise ValueError(f"Expected 'upload' signal, but received: {upload_signal}")
+            
+            metadata = socket.recv(1024).decode().strip()
+            file_info = self.parse_file_info(metadata)
+            self.received_files[file_id] = file_info
+            start_signal = socket.recv(1024).decode().strip()
+            if start_signal != "start":
+                raise ValueError(f"Expected 'start' signal, but received: {start_signal}")
+            
+            file_path = os.path.join(RECEIVED_DIR, file_id)
+            with open(file_path, 'wb') as f:
+                while True:
+                    data = socket.recv(4096)
+                    if not data:
+                        break
+                    f.write(data)
+        except Exception as e:
+            print(f"Error downloading file: {e}")
+        
+
+
+    def upload_file(self, file_id: str, socket: socket.socket):
+        print(f"Uploading file {file_id}")
+        socket.sendall(f"upload\n".encode())
+        file_info = self.shared_files_info[file_id]
+        metadata = self.format_file_info(file_info)
+        socket.sendall(f"{metadata}\n".encode())
+        socket.sendall("start\n".encode())
+        file_path = os.path.join(SHARED_DIR, file_id)
+        with open(file_path, 'rb') as f:
+            while True:
+                data = f.read(4096)
+                if not data:
+                    break
+                socket.sendall(data)
+        
--- src/peer.py
+++ src/peer.py
@@ -1,8 +1,19 @@
+from concurrent.futures import thread
+from os import name
 import socket
 import threading
 import time
+from typing import Tuple
 
-from src.file_service import FileService
+from src.file_service import FileInfo, FileService
+import logging
+
+
+logging.basicConfig(
+    filename="peer_connections.log",  
+    level=logging.INFO,              
+    format="%(asctime)s - %(levelname)s - %(message)s"  
+)
 
 UDP_PORT = 5001
 TCP_PORT = 5000
@@ -10,25 +21,28 @@ HEARTBEAT_INTERVAL = 3
 
 
 class Peer:
-    def __init__(self, server_address, local_peer_port):
+    def __init__(self, server_address, id):
         self.server_address = server_address
-        self.local_peer_port = local_peer_port
+        self.id=id
         self.file_service = FileService(self)
         self.running = False
-        self.peers = {}
+        self.peers = []
+        self.tcp_port = TCP_PORT
+        self.udp_port = UDP_PORT
 
     def start(self):
         self.running = True
-        self.register_with_server()
-        self.start_heartbeat_thread()
-        self.peers = self.request_peer_list()
+        #self.register_with_server()
+        #self.start_heartbeat_thread()
+        #self.peers = self.request_peer_list()
         self.file_service.start()
+        threading.Thread(target=self.listen_for_other_peers, daemon=True).start()
 
     def register_with_server(self):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((self.server_address, TCP_PORT))
-            s.sendall(f"register {self.local_peer_port}\n".encode())
+            s.sendall(f"register {self.id}\n".encode())
             response = s.recv(1024).decode()
             if response == "registered\n":
                 print("Registered with server")
@@ -43,7 +57,7 @@ class Peer:
             udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             while self.running:
                 try:
-                    udp_sock.sendto(f"heartbeat {self.local_peer_port}\n".encode(), (self.server_address, UDP_PORT))
+                    udp_sock.sendto(f"heartbeat {self.id}\n".encode(), (self.server_address, UDP_PORT))
                 except Exception as e:
                     print(f"Error sending heartbeat: {e}")
                 time.sleep(HEARTBEAT_INTERVAL)
@@ -63,10 +77,100 @@ class Peer:
             for peer in peer_list:
                 ip, port = peer.split(':')
                 peers.append((ip, int(port)))
+            self.peers = peers
             return peers
         except Exception as e:
             print(f"Error requesting peer list: {e}")
             return []
 
+    def _handle_search_request(self, client_socket, name):
+        try:
+            peer = client_socket.getpeername()
+            matching_files = self.file_service.get_shared_files_by_peer_and_file_name(peer,name)
+            logging.info(f"Found {len(matching_files)} matching files for {name} for {peer}")
+            files_info= []
+            for file_info in matching_files:
+                metadata = self.file_service.format_file_info(file_info)
+                files_info.append(metadata)
+
+            response = "\n".join(files_info)
+            client_socket.sendall(response.encode())
+        except Exception as e:
+            print(f"Error handling search request: {e}")
+        finally:
+            client_socket.close()
+
+    def _handle_download_request(self, client_socket, file_id):
+        self.file_service.upload_file(file_id, client_socket)
+        client_socket.close()
+
+    def _handle_upload_request(self, client_socket, file_id):
+        self.file_service.download_file(file_id, client_socket)
+        client_socket.close()
+
+
+    def listen_for_other_peers(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(('', self.tcp_port))
+        server_socket.listen(5)
+        print(f"Listening for other peers on port {self.tcp_port}")
+
+        server_socket.settimeout(4)
         
-    def 
+        while self.running:
+            try:
+                client_socket, address = server_socket.accept()
+                thread = threading.Thread(target=self._handle_peer_connection, args=(client_socket, address), daemon=True)
+                thread.start()
+            except socket.timeout:
+                continue
+            except Exception as e:
+                print(f"Error accepting connection: {e}")
+                time.sleep(1)  
+
+    
+    def _handle_peer_connection(self, client_socket: socket.socket, address: Tuple[str, int]):
+        try:
+            while True:
+                data = client_socket.recv(1024)
+                if not data:
+                    logging.info(f"Connection closed by {address}")
+                    break
+                command = data.decode()
+                logging.info(f"Received command from {address}: {command}")
+                if command.startswith("search"):
+                    self._handle_search_request(client_socket, command.split()[1])
+                    logging.info(f"Search request handled for {address}")
+                elif command.startswith("download"):
+                    self._handle_download_request(client_socket, command.split()[1])
+                    logging.info(f"Download request handled for {address}")
+                elif command.startswith("upload"):
+                    self._handle_upload_request(client_socket, command.split()[1])
+                    logging.info(f"Upload request handled for {address}")
+                break
+        except Exception as e:
+            print(f"Error handling peer connection: {e}")
+            logging.error(f"Error handling peer connection: {e}")
+        
+
+
+    def send_search_request_with_file_name(self, name)->list[FileInfo]:
+        try:
+            files = []
+            for peer in self.peers:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(4)
+                s.connect(peer)
+                s.sendall(f"search {name}".encode())
+                response = s.recv(4096).decode()
+                if not response:
+                    continue
+                files_info = response.strip().split('\n')
+                for file_info in files_info:
+                    metadata = self.file_service.parse_file_info(file_info)
+                    files.append(metadata)
+            return files
+        except Exception as e:
+            print(f"Error sending search request: {e}")
+        return []
